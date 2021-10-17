274064ffb527a5128738594e6315c4bd5751b648
alfi sahri <alfisahri33@gmail.com>
2021-10-18 03:20:54 +0700

    init

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+  private Rigidbody2D rig;
+
+  [Header("Movement")]
+  public float MoveAccel;
+  public float maxSpeed;
+
+  private void Start()
+  {
+    rig = GetComponent<Rigidbody2D>();
+  }
+
+  private void FixedUpdate()
+  {
+    Vector2 velocityVector = rig.velocity;
+    velocityVector.x = Mathf.Clamp(velocityVector.x + MoveAccel * Time.deltaTime, 0f, maxSpeed);
+
+    rig.velocity = velocityVector;
+  }
+
+}
