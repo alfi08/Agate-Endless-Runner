7797a11ba82fc96913ee5b2ada4f6164573011c7
alfi sahri <alfisahri33@gmail.com>
2021-10-18 04:47:38 +0700

    character walk and jump

--- Assets/Scripts/CameraMoveController.cs
+++ Assets/Scripts/CameraMoveController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CameraMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [Header("Position")]
+  public Transform player;
+  public float horizontalOffset;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private void Update()
+  {
+    Vector3 newPosition = transform.position;
+    newPosition.x = player.position.x + horizontalOffset;
+    transform.position = newPosition;
+  }
+
+
 }
--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -5,22 +5,83 @@ using UnityEngine;
 public class CharacterMoveController : MonoBehaviour
 {
   private Rigidbody2D rig;
+  private Animator anim;
+  private CharacterSoundController sound;
 
   [Header("Movement")]
   public float MoveAccel;
   public float maxSpeed;
 
+  [Header("Jump")]
+  public float jumpAccel;
+  private bool isJumping;
+
+  [Header("Ground Raycast")]
+  public float groundRaycastDistance;
+  public LayerMask groundLayerMask;
+  private bool isOnGround;
+
   private void Start()
   {
     rig = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
+    sound = GetComponent<CharacterSoundController>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Debug.Log("lompat " + isOnGround);
+      if (isOnGround)
+      {
+        isJumping = true;
+        sound.PlayJump();
+      }
+    }
+
+    // change animation
+    anim.SetBool("isOnGround", isOnGround);
   }
 
   private void FixedUpdate()
   {
+    RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+
+    if (hit)
+    {
+      Debug.Log("masuk");
+      if (!isOnGround && rig.velocity.y <= 0)
+      {
+        isOnGround = true;
+      }
+      else
+      {
+        isOnGround = false;
+      }
+    }
+    else
+    {
+      Debug.Log("engga");
+    }
+
+    // calculate velocity vector
     Vector2 velocityVector = rig.velocity;
+
+    if (isJumping)
+    {
+      velocityVector.y += jumpAccel;
+      isJumping = false;
+    }
+
     velocityVector.x = Mathf.Clamp(velocityVector.x + MoveAccel * Time.deltaTime, 0f, maxSpeed);
 
     rig.velocity = velocityVector;
+  }
+
+  private void OnDrawGizmos()
+  {
+    Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
   }
 
 }
